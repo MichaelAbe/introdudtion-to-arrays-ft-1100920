f0e387f9c9e4315060d141fb4d2edbd33e863270
MichaelAbe <michael.k.abe@gmail.com>
2020-11-12 13:38:44 -0500

    added code to arrays.rb

---
+++ README.md
@@ -0,0 +1,7 @@
+# Introduction to arrays
+
+
+### Steps to create a repository
+
+1. Created a folder to keep our project in
+2. Created a README.md 
---
+++ arrays.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+
+array = []
+
+name = "Bob"
+
+array.push(name)
+
+sarah = "Sarah"
+
+array.unshift(sarah)
+array.push("Tim")
+array.push("Lisa")
+
+binding.pry
